b6c0c8bcead007a243731c96d6038c22fdf4d11c
sanggu239 <romensch39@naver.com>
2022-07-25 15:37:39 +0900

    타깃넘버 add

--- algorithm/Level2/타깃넘버.py
+++ algorithm/Level2/타깃넘버.py
@@ -1,5 +1,8 @@
 
 
+from itertools import count
+
+
 def sol(numbers, target):
     main_node = [0]
 
@@ -8,12 +11,20 @@ def sol(numbers, target):
         for sub in main_node:
             sub_node.append(sub + num)
             sub_node.append(sub - num)
-
+    
         main_node = sub_node
-        print(main_node)
+
+
+    
+    return main_node.count(target)
 
 if __name__ == "__main__":
     a = [4, 1, 2, 1]
-    b = 2
+    b = 4
+    a = [1, 1, 1, 1, 1]	
+    b = 3
     ret = sol(a, b)
     print(ret)
+
+
+
